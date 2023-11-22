cdb969821e1f1ffcbc67bdccb6a0b18d38844218
Josselin <julien.josselin75@hotmail.fr>
2023-11-22 11:53:15 +0100

    pass migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_095408) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_103723) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
