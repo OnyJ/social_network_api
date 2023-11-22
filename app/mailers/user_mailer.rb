748f9ed4dad5893df28a9856dc7c2c73aaeef5a1
Josselin <julien.josselin75@hotmail.fr>
2023-11-22 10:28:11 +0100

    init repo

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: 'lawebsquad@gmail.com'
-
-  def welcome_email(user)
-    @user = user
-    @url = 'https://uclic.co/login/'
-    mail(to: @user.email, subject: 'Welcome to Uclic')
-  end
-end
--- db/migrate/20231006150143_enable_pgcrypto_extension.rb
+++
@@ -1,5 +0,0 @@
-class EnablePgcryptoExtension < ActiveRecord::Migration[7.0]
-  def change
-    enable_extension 'pgcrypto'
-  end
-end
