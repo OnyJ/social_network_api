965e5140297dd864a3875c70d5663d62d044f199
Josselin <julien.josselin75@hotmail.fr>
2023-11-22 11:35:49 +0100

    create and configure auth_controller

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,27 @@
+class AuthController < ApplicationController
+  skip_before_action :authorized, only: [:login]
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
+  def login
+    @user = User.find_by!(username: login_params[:username])
+    if @user.authenticate(login_params[:password])
+      @token = encode_token(user_id: @user.id)
+      render json: {
+        user: UserSerializer.new(@user),
+        token: @token
+      }, status: :accepted
+    else
+      render json: { message: 'Incorrect password' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:username, :password)
+  end
+
+  def handle_record_not_found(_error)
+    render json: { message: "User doesn't exist" }, status: :unauthorized
+  end
+end
